0c0936938179ac27bcdc5cb44af5b9c71bd08292
camilavenegass <cfv.suzarte@gmail.com>
2017-05-15 13:15:22 -0400

    Add files via upload

--- about.php
+++ about.php
@@ -7,7 +7,11 @@ array_walk($datos, function(&$a) use ($datos) {$a = array_combine($datos[0], $a)
 array_shift($datos);
 ?>
 <h2>Sobre el proyecto</h2>
-<p>Acá peguen sus about</p>
+<p>“La herida es la memoria del cuerpo; memoriza la fragilidad, el dolor, es decir, su existencia real. Es una defensa en contra del objeto y de las prótesis mentales.” - Gina Pane
+</p>
+<p>Constitución de la memoria corporal a través del dolor</p>
+<p>Tortura y violencia en Chile 1973-1990. </p>
+<p>Proyecto de Investigación sobre como las marcas provocadas en el cuerpo a través del dolor (tortura/flajelación) evocan una memoria en las victimas de la dictadura en Chile.</p>
 
 </section>
 <?php include('pie.php');?>
